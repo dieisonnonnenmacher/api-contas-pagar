2ab4ae322f7aee95fc472d0053cf3707ffa8a93d
Dieison Henrique Nonnenmacher <dieisonnonnenmacher@nb-004996.cwinet.local>
2020-12-25 15:59:05 -0300

    commit inicial

---
+++ src/main/java/br/com/apicontaspagar/ApiContasPagarApplication.java
@@ -0,0 +1,13 @@
+package br.com.apicontaspagar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiContasPagarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiContasPagarApplication.class, args);
+	}
+
+}
